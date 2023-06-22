abcac01b153bb55d12c7a5ea64737a93d9250214
solfany <solfany1999@gmail.com>
2023-06-22 16:23:09 +0900

    GalagaGame 수정

--- java/0622/로켓 미사일 미니 게임 /GalagaGame.java
+++ java/0622/로켓 미사일 미니 게임 /GalagaGame.java
@@ -1,5 +1,4 @@
 package ch16;
-
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
@@ -13,163 +12,272 @@ import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-//메인 클래스
 public class GalagaGame extends JPanel implements KeyListener {
-//게임 실행중 여부 플래그
-	private boolean running = true;
-//각 유닛 객체들 저장소
-	private ArrayList sprites = new ArrayList();
-	private Sprite starship;
-	private BufferedImage alienImage;
-	private BufferedImage shotImage;
-	private BufferedImage shipImage;
+    private boolean running = true;
+    private ArrayList<Sprite> sprites = new ArrayList<>();
+    private Sprite starship;
+    private BufferedImage alienImage;
+    private BufferedImage shotImage;
+    private BufferedImage shipImage;
 
-//생성자 : 프레임 셋팅 및 각 유닛 이미지 생성
-	public GalagaGame() {
-		JFrame frame = new JFrame("Galaga Game");
-		frame.setSize(800, 600);
-		frame.add(this);
-		frame.setResizable(false);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    // 미사일 저장소
+    private ArrayList<ShotSprite> shots = new ArrayList<>();
 
-		try {
-			shotImage = ImageIO.read(new File("/Users/kimsoulbee/Desktop/missile.png"));
-			shipImage = ImageIO.read(new File("/Users/kimsoulbee/Desktop/spaceship.png"));
-			alienImage = ImageIO.read(new File("/Users/kimsoulbee/Desktop/enemy.png"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-//포커스 지정(프레임)
-		this.requestFocus();
-//내 유닛과 적 유닛 생성하여 저장소에 저장
-		this.initSprites();
-//키 이벤트 지정(프레임)
-		addKeyListener(this);
-	}
-//내 유닛과 적 유닛 저장 메소드
-	private void initSprites() {
-//내 유닛 생성하여
-		starship = new StarShipSprite(this, shipImage, 370, 500);
-//저장소에 저장
-		sprites.add(starship);
-//적 유닛 객체를 5행 12열로 60마리 생성하여
-		for (int y = 0; y < 5; y++) {
-			for (int x = 0; x < 12; x++) {
-				Sprite alien = new AlienSprite(this, alienImage, 
-										00 + (x * 50), 50 + (y * 30));
-//순서대로 저장소에 저장
-				sprites.add(alien);
-			}
-		}
-	}
+    private int score = 0;
 
-//게임 시작 : 저장소를 비우고 다시 채움
-	private void startGame() {
-		sprites.clear();
-		initSprites();
-	}
+    public GalagaGame() {
+        JFrame frame = new JFrame("Galaga Game");
+        frame.setSize(800, 600);
+        frame.add(this);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-//게임 종료 : 프로그램 종료
-	public void endGame() {
-		System.out.println("endGame");
-		System.exit(0);
-	}
-//유닛 삭제 : 전달 받은 유닛을 저장소에서 삭제
-	public void removeSprite(Sprite sprite) {
-		sprites.remove(sprite);
-	}
-//탄환 생성 : 탄환 생성 후 저장소에 저장
-	public void fire() {
-		ShotSprite shot = new ShotSprite(this, shotImage, 
-				            starship.getX() + 10, starship.getY() - 30);
-		sprites.add(shot);
-	}
-//그리기 오버라이딩
-	@Override
-	public void paint(Graphics g) {
-//부모 컨테이너(프레임) 그리고
-		super.paint(g);
-//검은색 사각형 생성 (배경)
-		g.setColor(Color.black);
-		g.fillRect(0, 0, 800, 600);
-//저장소에 저장한 객체들을 모두 그리기 (제네릭 쓰면 형변환 안해도 됨)
-		for (int i = 0; i < sprites.size(); i++) {
-			Sprite sprite = (Sprite) sprites.get(i);
-			sprite.draw(g);
-		}
-	}
-//게임 진행
-	public void gameLoop() {
-//게임 실행중 여부 플래그에 따라 반복 진행/정지
-		while (running) {
-//저장소에 저장한 객체 모두 움직임
-			for (int i = 0; i < sprites.size(); i++) {
-				Sprite sprite = (Sprite) sprites.get(i);
-				sprite.move();
-			}
-//저장소의 모든 객체가 부딪혓는지 비교
-//예) 0번과 그 이후 나머지 비교, 1번과 그 이후 나머지 비교, .... 
-			for (int p = 0; p < sprites.size(); p++) {
-				for (int s = p + 1; s < sprites.size(); s++) {
-					Sprite me = (Sprite) sprites.get(p);
-					Sprite other = (Sprite) sprites.get(s);
-//두 객체가 부딪혔으면
-					if (me.checkCollision(other)) {
-//각 객체에 정의된 handleCollision 메소드로
-//적과 부딪혔는지 확인하여 그에 맞는 동작을 실행
-						me.handleCollision(other);
-						other.handleCollision(me);
-					}
-				}
-			}
-//변경된 정보로 다시 그리고 0.01초 쉼
-			repaint();
-			try {
-				Thread.sleep(10);
-			} catch (Exception e) {
-			}
-		}
-	}
+        try {
+            shotImage = ImageIO.read(new File("/Users/kimsoulbee/Desktop/missile.png"));
+            shipImage = ImageIO.read(new File("/Users/kimsoulbee/Desktop/spaceship.png"));
+            alienImage = ImageIO.read(new File("/Users/kimsoulbee/Desktop/enemy.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-//키 누름 이벤트
-	@Override
-	public void keyPressed(KeyEvent e) {
-//누른 키가 좌, 우 방향키이면 내 유닛 움직임
-		if (e.getKeyCode() == KeyEvent.VK_LEFT)
-			starship.setDx(-3);
-		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
-			starship.setDx(3);
-//누른 키가 스페이스바이면 탄환 생성하여 발사
-		if (e.getKeyCode() == KeyEvent.VK_SPACE)
-			fire();
-	}
+        this.requestFocus();
+        this.initSprites();
+        addKeyListener(this);
+    }
 
-//키 뗌 이벤트
-	@Override
-	public void keyReleased(KeyEvent e) {
-//좌우 방향키를 뗏으면 내 유닛 멈춤
-		if (e.getKeyCode() == KeyEvent.VK_LEFT)
-			starship.setDx(0);
-		if (e.getKeyCode() == KeyEvent.VK_LEFT)
-			starship.setDx(0);
-	}
+    private void initSprites() {
+        starship = new StarShipSprite(this, shipImage, 370, 500);
+        sprites.add(starship);
 
-	@Override
-	public void keyTyped(KeyEvent arg0) {
-	}
+        for (int y = 0; y < 5; y++) {
+            for (int x = 0; x < 12; x++) {
+                Sprite alien = new AlienSprite(this, alienImage, 00 + (x * 50), 50 + (y * 30));
+                sprites.add(alien);
+            }
+        }
+    }
 
-//프로그램 실행
-	public static void main(String[] args) {
-//이벤트 분배 스레드로 GUI와 각 객체 셋팅하고
-		GalagaGame g = new GalagaGame();
-//메인 스레드로 게임 진행
-		g.gameLoop();
-	}
+    private void startGame() {
+        sprites.clear();
+        initSprites();
+        score = 0;
+    }
+
+    public void endGame() {
+        System.out.println("endGame");
+        System.exit(0);
+    }
+
+    public void removeSprite(Sprite sprite) {
+        sprites.remove(sprite);
+    }
+
+    public void fire() {
+        ShotSprite shot = new ShotSprite(this, shotImage, starship.getX() + 10, starship.getY() - 30);
+        shots.add(shot);
+    }
+
+
+    // Q키
+    public void fireThreeShots() {
+          int shotSpacing = 25; // 탄환 간의 가로 간격
+
+    int startX = starship.getX() + 60 ; // 시작 위치: 현재 우주선의 가로 위치
+    int startY = starship.getY() - 30; // 시작 위치: 우주선 위쪽으로 30만큼 이동
+
+        if (shots.size() < 3) {
+            ShotSprite shot1 = new ShotSprite(this, shotImage, starship.getX() + 5, starship.getY() - 30);
+            ShotSprite shot2 = new ShotSprite(this, shotImage, starship.getX() + 10, starship.getY() - 30);
+            ShotSprite shot3 = new ShotSprite(this, shotImage, starship.getX() + 15, starship.getY() - 30);
+
+            shots.add(shot1);
+            shots.add(shot2);
+            shots.add(shot3);
+        }
+          for (int i = 0; i < 3; i++) {
+        int shotX = startX - (shotSpacing * (i - 1)); // 탄환의 가로 위치 계산
+        ShotSprite shot = new ShotSprite(this, shotImage, shotX, startY);
+        sprites.add(shot);
+    }
+}
+
+    // W키
+    public void fireThreeShots1() {
+          int shotSpacing = 50; // 탄환 간의 가로 간격
+
+    int startX = starship.getX() + 60 ; // 시작 위치: 현재 우주선의 가로 위치
+    int startY = starship.getY() - 30; // 시작 위치: 우주선 위쪽으로 30만큼 이동
+
+        if (shots.size() < 5) {
+            ShotSprite shot1 = new ShotSprite(this, shotImage, starship.getX() + 5, starship.getY() - 30);
+            ShotSprite shot2 = new ShotSprite(this, shotImage, starship.getX() + 10, starship.getY() - 30);
+            ShotSprite shot3 = new ShotSprite(this, shotImage, starship.getX() + 15, starship.getY() - 30);
+
+            shots.add(shot1);
+            shots.add(shot2);
+            shots.add(shot3);
+        }
+          for (int i = 0; i < 5; i++) {
+        int shotX = startX - (shotSpacing * (i - 1)); // 탄환의 가로 위치 계산
+        ShotSprite shot = new ShotSprite(this, shotImage, shotX, startY);
+        sprites.add(shot);
+    }
+}
+// E키
+public void fireThreeShots3() {
+    int shotSpacing = 50; // 탄환 간의 가로 간격
+    int startX = starship.getX() + 60; // 시작 위치: 현재 우주선의 가로 위치
+    int startY = starship.getY() - 30; // 시작 위치: 우주선 위쪽으로 30만큼 이동
+
+    if (shots.size() < 1) { // 한 번에 하나의 흰색 동그라미 미사일만 발사하도록 수정
+        ShotSprite shot = new WhiteCircleShotSprite(this, shotImage, startX, startY);
+        shots.add(shot);
+    }
+
+    for (int i = 0; i < shots.size(); i++) {
+        ShotSprite shot = shots.get(i);
+        if (!shot.isCollided()) { // 충돌 후 딜레이 중인 미사일은 체크하지 않음
+            for (int j = 0; j < sprites.size(); j++) {
+                Sprite sprite = sprites.get(j);
+                if (sprite instanceof AlienSprite && shot.checkCollision(sprite)) {
+                    score++;
+                    shot.setCollided(true);
+                    sprites.remove(j);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+// 흰색 동그라미 미사일을 나타내는 ShotSprite 클래스를 상속받은 WhiteCircleShotSprite 클래스를 추가
+public class WhiteCircleShotSprite extends ShotSprite {
+    private int collisionDelay = 20; // 충돌 후 딜레이 시간
+
+    public WhiteCircleShotSprite(GalagaGame game, BufferedImage image, int x, int y) {
+        super(game, image, x, y);
+    }
+
+    @Override
+    public void move() {
+        if (!isCollided()) {
+            super.move();
+        } else {
+            if (collisionDelay > 0) {
+                collisionDelay--;
+            } else {
+                setCollided(false);
+                collisionDelay = 20; // 딜레이 시간 초기화
+            }
+        }
+    }
 }
 
 
-//스코어 넣기 
-//기능 추가 
 
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.setColor(Color.black);
+        g.fillRect(0, 0, 800, 600);
+
+        for (Sprite sprite : sprites) {
+            sprite.draw(g);
+        }
+
+        for (ShotSprite shot : shots) {
+            shot.draw(g);
+        }
+
+        g.setColor(Color.white);
+        g.drawString("Score: " + score, 10, 20);
+    }
+
+    public void gameLoop() {
+        while (running) {
+            for (int i = 0; i < sprites.size(); i++) {
+                Sprite sprite = sprites.get(i);
+                sprite.move();
+            }
+
+            for (int p = 0; p < sprites.size(); p++) {
+                for (int s = p + 1; s < sprites.size(); s++) {
+                    Sprite me = sprites.get(p);
+                    Sprite other = sprites.get(s);
+
+                    if (me.checkCollision(other)) {
+                        me.handleCollision(other);
+                        other.handleCollision(me);
+                    }
+                }
+            }
+
+            // Update and remove shots
+            for (int i = shots.size() - 1; i >= 0; i--) {
+                ShotSprite shot = shots.get(i);
+                shot.move();
+                if (shot.getY() < 0) {
+                    shots.remove(i);
+                }
+            }
+
+            // Check collision between shots and aliens
+            for (int i = 0; i < shots.size(); i++) {
+                ShotSprite shot = shots.get(i);
+                for (int j = 0; j < sprites.size(); j++) {
+                    Sprite sprite = sprites.get(j);
+                    if (sprite instanceof AlienSprite && shot.checkCollision(sprite)) {
+                        score++;
+                        shots.remove(i);
+                        sprites.remove(j);
+                        break;
+                    }
+                }
+            }
+
+            repaint();
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT)
+            starship.setDx(-3);
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+            starship.setDx(3);
+        if (e.getKeyCode() == KeyEvent.VK_SPACE)
+            fire();
+        if (e.getKeyCode() == KeyEvent.VK_Q)
+            fireThreeShots();
+    
+        if (e.getKeyCode() == KeyEvent.VK_W)
+            fireThreeShots1();
+
+        if (e.getKeyCode() == KeyEvent.VK_S)
+            fireThreeShots();
+        }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT)
+            starship.setDx(0);
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+            starship.setDx(0);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    public static void main(String[] args) {
+        GalagaGame game = new GalagaGame();
+        game.gameLoop();
+    }
+}
 
